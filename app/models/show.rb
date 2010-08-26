6693d739fdc75c4f4dd6b1fc162c60ea4b696ac2
Jared Wilkerson <wilkersonj@sil131.(none)>
2010-08-26 15:21:52 -0400

    added validations for shows and songs, but need to create separate validation function for validating the combination of a song's artist and title

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,5 @@
 class Show < ActiveRecord::Base
+  validates_presence_of :title, :location, :picture, :description
+  #validates_numericality_of TODO need to add the date to the show table
+  validates_uniqueness_of :title,:location #TODO need to change title to episode title
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,5 @@
 class Song < ActiveRecord::Base
+  validates_presence_of :title, :genre, :artist, :album
+  #validates_numericality_of
+  validates_uniqueness_of :title
 end
