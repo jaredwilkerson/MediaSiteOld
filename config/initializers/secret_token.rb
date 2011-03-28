16a60ffa6d51cd12677bb359c360fa20d3c32942
Jared Wilkerson <jaredwilkerson@jared-wilkersons-macbook.local>
2011-03-28 05:31:01 -0400

    forgot to add some files for the rails3 update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaSite::Application.config.secret_token = '4f3a8bcd2b8ffb990d059d5bc82eb18ebe45991ca4d9d558a042913a9f48f9a1db286f0e8c2b36b681823a395ccf5fc64156fa02c06ed788e4c0e8177d3a6829'
